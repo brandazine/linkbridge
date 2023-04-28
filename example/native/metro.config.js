beadf7c50da83b88951788731dea68329256b494
gronxb <gron1gh1@gmail.com>
2023-04-28 23:51:10 +0900

    chore(example): metro symlinks

--- example/native/metro.config.js
+++ example/native/metro.config.js
@@ -1,21 +1,8 @@
-const {getDefaultConfig} = require('metro-config');
 const {makeMetroConfig} = require('@rnx-kit/metro-config');
 const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
-const path = require('path');
 
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig();
-
-  const config = makeMetroConfig({
-    projectRoot: path.resolve(__dirname),
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-      resolveRequest: MetroSymlinksResolver(),
-      useWatchman: false,
-    },
-  });
-  return config;
-})();
+module.exports = makeMetroConfig({
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+});
