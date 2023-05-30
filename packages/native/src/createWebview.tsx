3a5ab2bfc6a0e684cc4c91d43f6b47c9f3b91d7a
gronxb <gron1gh1@gmail.com>
2023-05-30 12:53:14 +0900

    feat(native): createWebview hoist scope

--- packages/native/src/createWebview.tsx
+++ packages/native/src/createWebview.tsx
@@ -1,7 +1,7 @@
 import WebView, {type  WebViewProps } from 'react-native-webview';
 import type { Procedure, ProceduresObject } from './integrations';
 import { INTEGRATIONS_SCRIPTS_CONSOLE, INTEGRATIONS_SCRIPTS_BRIDGE, handleLog, handleBridge } from './integrations';
-import { createRef } from 'react';
+import { useMemo, useRef } from 'react';
 
 type LinkBridgePersist = {
   localStorage: string[]
@@ -15,39 +15,44 @@ interface CreateWebviewArgs {
 }
 
 export const createWebview = ({ bridge, host, debug }: CreateWebviewArgs) => {
-  const webviewRef = createRef<WebView>();
-
-  const bridgeNames = Object.values(bridge ?? {})
-    .map((func) => {
-      return `'${func.name}'`;
-    })
-    .join();
-
   return {
-    Webview: (props: WebViewProps) => <WebView 
-    ref={webviewRef}
-    source={{ uri: host }}
-    onMessage={async (event) => {
-      const {method, args, type, eventId} = JSON.parse(event.nativeEvent.data);
+    Webview: (props: WebViewProps) => {
+      const webviewRef = useRef<WebView>(null);
 
-      switch(type) {
-        case 'log':
-          debug && handleLog(method, args);
-          return;
-        case 'bridge':
-          webviewRef.current && handleBridge({
-            bridge,
-            method,
-            args,
-            eventId,
-            webview: webviewRef.current,
-          });
-          return;
-      }
-    }}
-    injectedJavaScriptBeforeContentLoaded={[INTEGRATIONS_SCRIPTS_BRIDGE(bridgeNames), 'true;'].filter(Boolean).join('\n')}
-    injectedJavaScript={[console && INTEGRATIONS_SCRIPTS_CONSOLE, 'true;'].filter(Boolean).join('\n')}
-    
-    {...props} />
+      const bridgeNames = useMemo(() => Object.values(bridge ?? {})
+        .map((func) => {
+          return `'${func.name}'`;
+        })
+        .join(), [bridge]);
+
+        
+      return <WebView 
+                ref={webviewRef}
+                source={{ uri: host }}
+                onMessage={async (event) => {
+                  const {method, args, type, eventId} = JSON.parse(event.nativeEvent.data);
+
+                  switch(type) {
+                    case 'log':
+                      debug && handleLog(method, args);
+                      return;
+                    case 'bridge':
+                      webviewRef.current && handleBridge({
+                        bridge,
+                        method,
+                        args,
+                        eventId,
+                        webview: webviewRef.current,
+                      });
+                      return;
+                  }
+                }}
+                injectedJavaScriptBeforeContentLoaded={[INTEGRATIONS_SCRIPTS_BRIDGE(bridgeNames), 'true;'].filter(Boolean).join('\n')}
+                injectedJavaScript={[console && INTEGRATIONS_SCRIPTS_CONSOLE, 'true;'].filter(Boolean).join('\n')}
+                onNavigationStateChange={(navState) => {
+                  console.log("navState", navState);
+                }}
+                {...props} />
+  }
   };
 };
