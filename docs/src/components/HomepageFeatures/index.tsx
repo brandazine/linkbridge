ff1fc98f93a6882371748e328a4ff3d4f2e82721
gronxb <gron1gh1@gmail.com>
2023-05-30 14:23:20 +0900

    chore(docs): setup docusaurus

---
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,61 @@
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import React from "react";
+
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Easy to Use",
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
+        ahead and move your docs into the <code>docs</code> directory.
+      </>
+    ),
+  },
+  {
+    title: "Powered by React",
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function Feature({ title, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <Link className="button button--secondary button--lg" to="/docs/intro">
+        Getting Started
+      </Link>
+    </section>
+  );
+}
